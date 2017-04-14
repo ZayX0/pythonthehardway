cfef73254a2162ec120d0f383250f5a24b413be2
Isaiah Reed <ireed@indeed.com>
2017-04-14 10:38:23 -0500

    A couple new things

--- ex33.py
+++ ex33.py
@@ -1,20 +1,30 @@
-i = 0
-numbers = []
-
-def my_loop(i, pass_number):
-    while i < pass_number:
+"""def my_loop(i, pass_number, jumper):
+    while i <= pass_number:
         print "At the top i is %d" % i
         numbers.append(i)
 
-        i += 1
+        i += jumper
         print "Numbers now: ", numbers
-        print "At the bottom i is %d" % i
+        print "At the bottom i is %d" % i """
 
-print "How many teams do you have?"
-teams = int(raw_input("> "))
+def my_for_loop(i, run_amt):
+    for i in range(0,run_amt):
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-my_loop(i, teams)
+        print "Numbers now: ", numbers
+
+print "What number should I stop at?"
+run_time = int(raw_input("> "))
+
+print "How much should I count up by?"
+count_up_by = int(raw_input("> "))
+
+i = 0
+numbers = []
+
+my_for_loop(i, run_time)
 print "The numbers: "
 
 for num in numbers:
-    print num
+    print num,
